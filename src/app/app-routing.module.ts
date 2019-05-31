2a070e34e0720668631e41c49f8cc6a498ccb494
Nemanja <savicn492@gmail.com>
2019-05-31 15:26:12 -0700

    Created login and registration frontend.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+
+import { AuthguardService } from './services/authguard.service';
+
+
+const routes: Routes = [
+	{path: '', component: LoginComponent},
+	{path: 'login', component: LoginComponent},
+	{path: 'registration', component: RegistrationComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+
+import { RegistrationService } from './services/registration.service';
+import { LoginService } from './services/login.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegistrationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [RegistrationService, LoginService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
