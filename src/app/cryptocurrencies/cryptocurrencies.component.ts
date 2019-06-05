8e276d9756e535876b6a80d158c57c60539c5b16
Nemanja <savicn492@gmail.com>
2019-06-05 10:42:09 -0700

    Generated api link with params.

--- src/app/cryptocurrencies/cryptocurrencies.component.ts
+++ src/app/cryptocurrencies/cryptocurrencies.component.ts
@@ -7,21 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CryptocurrenciesComponent implements OnInit {
 
-  cryptocurrenciesApi = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=ETH,DASH&tsyms=BTC,USD,EUR&api_key=34d57fe96dc0d6ac74a21983e960ead7a35189dedc28c92abf33544d6f809169";
-
-  apiKey = "34d57fe96dc0d6ac74a21983e960ead7a35189dedc28c92abf33544d6f809169";
-
-  fromValutes = ["BTC", "XRP", "ETH", "LTC"];
-  toValutes = ["USD", "EUR", "BTC",  "XRP", "ETH", "LTC"];
+  CONVERT_CRYPTOCURRENCIES_API = "https://min-api.cryptocompare.com/data/pricemulti";
+  API_KEY = "34d57fe96dc0d6ac74a21983e960ead7a35189dedc28c92abf33544d6f809169";
+  
+  FROM_VALUTES = ["BTC", "XRP", "ETH", "LTC"];
+  TO_VALUTES = ["USD", "EUR", "BTC",  "XRP", "ETH", "LTC"];
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  convertCriptocurrencies(){
-  	//alert("Submiting...");
-  	//console.log(JSON.stringify(document.querySelectorAll('#selectBoxTo option:checked');));
+  convertCriptocurrencies() {
+  	let from = this.getSelectedOptions("selectBoxFrom");
+  	let to = this.getSelectedOptions("selectBoxTo");
+  	let apiRequest = this.getApiConvertValutesRequest(from, to);
+	
+  	
+	
+  }
+
+  getSelectedOptions(selectBoxId) {
+  	let selectBox = document.getElementById(selectBoxId);
+    let selectedOptions = [];
+    for (let i = 0; i < selectBox.length; i++) {
+        if (selectBox.options[i].selected) selectedOptions.push(selectBox.options[i].value);
+    }
+
+    return selectedOptions;
+  }
+
+  getApiConvertValutesRequest(from, to) {
+  	let paramFromValutes = "fsyms=" + from.join();
+  	let paramToValutes = "tsyms=" + to.join();
+  	let paramApiKey = "api_key=" + this.API_KEY;
+
+  	return this.CONVERT_CRYPTOCURRENCIES_API + "?" + paramFromValutes + "&" + paramToValutes + paramApiKey;
   }
 
 }
