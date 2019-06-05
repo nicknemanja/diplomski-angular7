8d4a940fac1ac5d1d9377c7c91e7b0193b488347
Nemanja <savicn492@gmail.com>
2019-06-05 04:59:09 -0700

    Creating cryptocurrencies component.

--- src/app/cryptocurrencies/cryptocurrencies.component.ts
+++ src/app/cryptocurrencies/cryptocurrencies.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CryptocurrenciesComponent implements OnInit {
 
+  cryptocurrenciesApi = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=ETH,DASH&tsyms=BTC,USD,EUR&api_key=34d57fe96dc0d6ac74a21983e960ead7a35189dedc28c92abf33544d6f809169";
+
+  apiKey = "34d57fe96dc0d6ac74a21983e960ead7a35189dedc28c92abf33544d6f809169";
+
+  fromValutes = ["BTC", "XRP", "ETH", "LTC"];
+  toValutes = ["USD", "EUR", "BTC",  "XRP", "ETH", "LTC"];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  convertCriptocurrencies(){
+  	//alert("Submiting...");
+  	//console.log(JSON.stringify(document.querySelectorAll('#selectBoxTo option:checked');));
   }
 
 }
